0e77e21f0d5cf97c62eca27958015fb106a1845a
MoBoudni <168630329+MoBoudni@users.noreply.github.com>
2025-05-17 00:47:28 +0200

    Update Person.java
    Diese Klasse repräsentiert eine Person mit den Eigenschaften Name, Alter, Standort und Geschlecht. Sie enthält einen Konstruktor zur Initialisierung dieser Eigenschaften sowie Getter-Methoden zum Abrufen der Werte.

--- OOP_Generics/src/de/generics/Person.java
+++ OOP_Generics/src/de/generics/Person.java
@@ -1,33 +1,64 @@
 package de.generics;
 
+/**
+ * Diese Klasse repräsentiert eine Person mit Eigenschaften wie Name, Alter, Standort und Geschlecht.
+ */
 public class Person {
-	
-	private String name;
-	private int age;
-	private String standort;
-	private String geschlecht;
-	
-	
-	
-	public Person(String name, int age, String standort, String geschlecht) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.standort = standort;
-		this.geschlecht = geschlecht;
-		
-	}
-	public String getName() {
-		return name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public String getStandort() {
-		return standort;
-	}
-	public String getGeschlecht() {
-		return geschlecht;
-	}
 
+    // Private Member-Variablen
+    private String name;
+    private int age;
+    private String standort;
+    private String geschlecht;
+
+    /**
+     * Konstruktor zur Initialisierung eines Person-Objekts.
+     *
+     * @param name      der Name der Person
+     * @param age       das Alter der Person
+     * @param standort  der Standort der Person
+     * @param geschlecht das Geschlecht der Person
+     */
+    public Person(String name, int age, String standort, String geschlecht) {
+        this.name = name;
+        this.age = age;
+        this.standort = standort;
+        this.geschlecht = geschlecht;
+    }
+
+    /**
+     * Gibt den Namen der Person zurück.
+     *
+     * @return der Name der Person
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gibt das Alter der Person zurück.
+     *
+     * @return das Alter der Person
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Gibt den Standort der Person zurück.
+     *
+     * @return der Standort der Person
+     */
+    public String getStandort() {
+        return standort;
+    }
+
+    /**
+     * Gibt das Geschlecht der Person zurück.
+     *
+     * @return das Geschlecht der Person
+     */
+    public String getGeschlecht() {
+        return geschlecht;
+    }
 }
