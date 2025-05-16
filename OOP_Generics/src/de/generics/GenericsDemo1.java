6d1c2b0ea3f6d4adbf9251a0e6a412503dc9ad93
MoBoudni <168630329+MoBoudni@users.noreply.github.com>
2025-05-17 00:46:22 +0200

    Update GenericsDemo1.java
    Diese Klasse enthält die Hauptmethode, die eine Liste von Person-Objekten erstellt und deren Eigenschaften anzeigt. Sie zeigt, wie man eine ArrayList verwendet, um Objekte eines bestimmten Typs zu speichern und zu verarbeiten.

--- OOP_Generics/src/de/generics/GenericsDemo1.java
+++ OOP_Generics/src/de/generics/GenericsDemo1.java
@@ -2,27 +2,37 @@ package de.generics;
 
 import java.util.ArrayList;
 
+/**
+ * Diese Klasse demonstriert die Verwendung von Generics in Java.
+ * Sie erstellt eine Liste von Person-Objekten und zeigt deren Eigenschaften an.
+ */
 public class GenericsDemo1 {
-	
-	public static void main (String args[]) {
-		
-		ArrayList<Person> persons = new ArrayList<>();
-		
-		persons.add( new Person("Peter", 31, "Köln", "m") );
-		persons.add( new Person("Peterson", 41, "Düsseldorf", "m") );
-		persons.add( new Person("Claudia", 27, "Bonn", "w") );
-		persons.add( new Person("Petra", 34, "Trier", "w") );
-		
-		for (int i = 0; i < persons.size(); i++) {
-			
-			Person somebody = (Person) persons.get(i);
-			
-			System.out.println("Name: " + somebody.getName());
-			System.out.println("Alter: " + somebody.getAge());
-			System.out.println("Standort: " + somebody.getStandort());
-			System.out.println("Geschlecht: " + somebody.getGeschlecht());
-			System.out.println();
-			
-		}
-	}
+
+    /**
+     * Die Hauptmethode, die das Programm startet.
+     *
+     * @param args Kommandozeilenargumente (nicht verwendet)
+     */
+    public static void main(String args[]) {
+
+        // Erstellen einer ArrayList zur Speicherung von Person-Objekten
+        ArrayList<Person> persons = new ArrayList<>();
+
+        // Hinzufügen von Person-Objekten zur Liste
+        persons.add(new Person("Peter", 31, "Köln", "m"));
+        persons.add(new Person("Peterson", 41, "Düsseldorf", "m"));
+        persons.add(new Person("Claudia", 27, "Bonn", "w"));
+        persons.add(new Person("Petra", 34, "Trier", "w"));
+
+        // Iterieren durch die Liste und Anzeigen der Eigenschaften jeder Person
+        for (int i = 0; i < persons.size(); i++) {
+            Person somebody = persons.get(i);
+
+            System.out.println("Name: " + somebody.getName());
+            System.out.println("Alter: " + somebody.getAge());
+            System.out.println("Standort: " + somebody.getStandort());
+            System.out.println("Geschlecht: " + somebody.getGeschlecht());
+            System.out.println();
+        }
+    }
 }
